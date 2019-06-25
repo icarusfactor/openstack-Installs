dce731aac7faf4772281dd60f7e6e56589472e95
icarusfactor aka Daniel Yount <factorf2@yahoo.com>
2019-06-25 03:40:45 -0500

    .gitignore fix

---
+++ stein/create_project_web_scale.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+#Create image as admin
+./create_osc_img.sh
+
+#Create user as admin and key as user
+./create_project_user_key.sh
+
+#Create secuirty groups for network
+./create_privnet_secgroups.sh
+
+#Create private subnet and connect it to the exteranl router. 
+./create_privnet.sh
+
+#Create all three boxes for the web control system.
+./launch_img_to_instance.sh
+
+#This is just a text on how tomanually  setup ssh forwarding and
+# get to all the systems in the group. So you can add or change
+# user that this is for.  
+sh_fwd_test.sh
