4117b6b14749c3716974021ed8e51da035f52d1e
icarusfactor aka Daniel Yount <factorf2@yahoo.com>
2019-08-25 17:21:32 -0500

    fixed apche port proxy 8080

--- puppet/apache2.pp
+++ puppet/apache2.pp
@@ -1,6 +1,13 @@
-class { 'apache':}
+class { 'apache':
+        default_vhost => true,
+}
+
+apache::vhost { 'proxy':
+  port    => '8080',
+  servername => 'proxy',
+  docroot => '/var/www/',
+}
 
 include ::apache::mod::php
 apache::listen { '8080': }
-
 
